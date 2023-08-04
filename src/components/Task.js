2bd009c72509862a076c86ea4ffef273ce4dd53f
BreElle Wells <breellejordyn@gmail.com>
2023-08-03 19:15:52 -0600

    complete task deliverable

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 
-function Task({ text, category }) {
+function Task({ text, category, handleClick }) {
   return (
     <div className="task">
       <div className="label">{category}</div>
       <div className="text">{text}</div>
-      <button className="delete">X</button>
+      <button className="delete" onClick={() => handleClick(text)}>X</button>
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,23 @@
 import React, { useState } from "react";
 import Task from "./Task";
 
+
 function TaskList({ tasks }) {
+  const [tasksArray, setTasksArray] = useState(tasks)
+
+  function handleClick(id) {
+    const updatedTasks = tasksArray.filter((task) => {
+      if (task.text != id) {
+        return true;
+      }
+    })
+    setTasksArray(updatedTasks)
+  }
+
   return (
     <div className="tasks">
-      {tasks.map((task) => (
-        <Task key={task.name} text={task.text} category={task.category} />
+      {tasksArray.map((task) => (
+        <Task key={task.text} text={task.text} category={task.category} handleClick={handleClick} />
       ))
       }
     </div>
