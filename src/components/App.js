88445e56e988e107d609d4cb571c1509d438559c
BreElle Wells <breellejordyn@gmail.com>
2023-08-03 19:29:26 -0600

    create category buttons

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,12 @@ import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
 
 import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
 
 function App() {
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
+      <CategoryFilter categories={CATEGORIES} />
       <NewTaskForm />
       <TaskList tasks={TASKS} />
     </div>
--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CategoryFilter() {
+function CategoryFilter({ categories }) {
+  console.log(categories)
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {categories.map((category) => (
+        <button key={category}>{category}</button>
+      ))}
     </div>
   );
 }
+
 
 export default CategoryFilter;
