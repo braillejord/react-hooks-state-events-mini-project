ee850ac554f18653558d08bfd234c680a1450bd1
BreElle Wells <breellejordyn@gmail.com>
2023-08-03 18:40:46 -0600

    complete task list deliverable

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({ tasks }) {
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {tasks.map((task) => (
+        <Task key={task.name} text={task.text} category={task.category} />
+      ))
+      }
     </div>
   );
 }
