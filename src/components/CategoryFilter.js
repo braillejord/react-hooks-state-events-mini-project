42ee9a5d9db0bd417f44f69c49b356d638edda7b
BreElle Wells <breellejordyn@gmail.com>
2023-08-04 07:19:37 -0600

    filter tasks by category

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,17 +1,23 @@
 import React, { useState } from "react";
 
-function CategoryFilter({ categories }) {
-  console.log(categories)
+function CategoryFilter({ categories, getCategory }) {
+  const [categoryArray, setCategoryArray] = useState(categories)
+  const [selectedCat, setSelectedCat] = useState();
+
+  function updateCategory(category) {
+    setSelectedCat(category)
+    getCategory(category)
+  }
+
 
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {categories.map((category) => (
-        <button key={category}>{category}</button>
+      {categoryArray.map((category) => (
+        <button key={category} onClick={() => updateCategory(category)} className={selectedCat === category ? "selected" : undefined}>{category}</button>
       ))}
     </div>
   );
 }
-
 
 export default CategoryFilter;
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -2,8 +2,9 @@ import React, { useState } from "react";
 import Task from "./Task";
 
 
-function TaskList({ tasks }) {
+function TaskList({ tasks, selectedCategory }) {
   const [tasksArray, setTasksArray] = useState(tasks)
+  console.log(selectedCategory)
 
   function handleClick(id) {
     const updatedTasks = tasksArray.filter((task) => {
@@ -14,9 +15,18 @@ function TaskList({ tasks }) {
     setTasksArray(updatedTasks)
   }
 
+  const taskToDisplay = tasks
+    .filter((task) => {
+      if (selectedCategory === "All") {
+        return true;
+      } else if (task.category === selectedCategory) {
+        return true;
+      }
+    })
+
   return (
     <div className="tasks">
-      {tasksArray.map((task) => (
+      {taskToDisplay.map((task) => (
         <Task key={task.text} text={task.text} category={task.category} handleClick={handleClick} />
       ))
       }
